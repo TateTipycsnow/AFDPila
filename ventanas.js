d427d46d06bc05053a3b8b5c6d16c6955c337fd3
Jared Salazar Martinez <33723377+TateTipycsnow@users.noreply.github.com>
2020-11-21 20:27:28 -0600

    Ventanas hechas

--- ventanas.js
+++ ventanas.js
@@ -1,2 +1,31 @@
 const { BrowserWindow } = require('electron').remote;
 const path = require('path');
+
+const firstButton = document.getElementById('1erAFD');
+const secondButton = document.getElementById('2ndAFD');
+
+firstButton.addEventListener('click', (event) => {
+    const afd1er = path.join('file://', __dirname, '/windows/1erAFD.html');
+
+    let ventana1 = new BrowserWindow({
+        width: 700,
+        height: 500
+    });
+
+    ventana1.on('close', () => { ventana1 = null });
+    ventana1.loadURL(afd1er);
+    ventana1.show();
+});
+
+secondButton.addEventListener('click', (event) => {
+    const afd2nd = path.join('file://', __dirname, '/windows/2ndAFD.html');
+
+    let ventana2 = new BrowserWindow({
+        width: 700,
+        height: 500
+    });
+
+    ventana2.on('close', () => { ventana2 = null });
+    ventana2.loadURL(afd2nd);
+    ventana2.show();
+});
